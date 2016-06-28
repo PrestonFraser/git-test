96c08a525bc18fdfc470b23bbbeaf8ddbf2e1acf
PrestonFraser <pfraser@salestreamsoft.com>
2016-06-28 10:16:40 -0700

    Adding the core script file to the repository

---
+++ gitJavaTest.java
@@ -0,0 +1,17 @@
+/*******************************************************************
+ * Preston Fraser
+ * gitJavaTest.java
+ * 6/28/2017
+ * Test file for learning git hub.
+*/
+
+public class gitJavaTest{
+
+	public void main(String args[]){
+
+		System.out.printf("Hello, Preston.");
+		return;
+
+	}
+
+}
